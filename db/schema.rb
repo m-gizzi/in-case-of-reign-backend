084c2313ede56cdf99de6a80b9c57c66a7abe4fd
m-gizzi <matthewgizzi@verizon.net>
2020-04-22 18:01:29 -0500

    Changed db to pgsql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_04_162310) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bundles", force: :cascade do |t|
     t.string "theme"
     t.datetime "created_at", precision: 6, null: false
